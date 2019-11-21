968c932c577fc03ee8ccb8530a8726739102ab8c
I-Shadow-I <57918832+I-Shadow-I@users.noreply.github.com>
2019-11-21 20:04:46 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,51 @@
+
+let add =(t,container=document.body)=>{
+  e = document.createElement(t);
+  container.appendChild(e);
+  return e;}
+
+let btn = document.getElementById("LoveButtom")
+
+
+// create balloon
+const create_love = function(x,y){
+  let love_img = document.createElement('img');
+  love_img.src = 'image/coeur.png';
+  love_img.style.height = '50px';
+  love_img.style.width = '50px';
+  love_img.style.position = 'fixed';
+  love_img.style.left = x + 'px';
+  love_img.style.top = y + 'px';
+  document.body.appendChild(love_img)
+  return love_img
+}
+
+// move the balloon
+const move_love = function(el){
+  let y = parseInt(el.style.top)
+  y += 1;
+  el.style.top = y + 'px';
+  if(y < 700){
+    setTimeout(()=> move_love(el), 20)}
+  else{
+    document.body.removeChild(el);
+  }
+}
+
+const random_number = function(max){
+		return Math.ceil(Math.random()*max);
+}
+
+mult = function(times, f){
+	if(times <= 0){
+		return}
+	else{
+		f()
+		mult(times-1, f)}}
+
+
+btn.onclick = function(){
+  mult(10, ()=>{
+    let bal= create_love(random_number(1500),random_number(300));
+    move_love(bal);});
+}
